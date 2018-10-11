b13a663b9506d613dc2a04062e1c88ee00e266ea
Gavin Price <gaprice@lbl.gov>
2018-10-10 19:00:58 -0700

    List groups

--- testui/js/Groups.js
+++ testui/js/Groups.js
@@ -25,6 +25,7 @@ export default class {
         <input id="url"/><button id="seturl">Set</button><span id="servroot"></span>
       </div>
       <div id="error"></div>
+      <div id="groups"></div>
     `;
 
     rootElement.innerHTML = html;
@@ -51,35 +52,72 @@ export default class {
       alert("Token: " + this.token);
   }
   
+  epochToDate(epoch) {
+      const d = new Date(0);
+      d.setUTCMilliseconds(epoch);
+      return d;
+  }
+  
   setURL() {
       $('#error').text("");
       $('#servroot').text("");
-      var url = $('#url').val();
+      let url = $('#url').val();
       fetch(url)
          .then( (response) => {
              if (response.ok) {
                  response.json().then( (json) => {
-                     var d = new Date(0);
-                     d.setUTCMilliseconds(json.servertime);
+                     const d = this.epochToDate(json.servertime);
                      $('#servroot').html(
                              `<strong>Service name</strong>: ${json.servname} ` +
                              `<strong>version</strong>: ${json.version} ` + 
                              `<strong>time</strong>: ${d} ` +
                              `<strong>commit</strong>: ${json.gitcommithash} `
                              );
+                     if (!url.endsWith('/')) {
+                         url = url + '/'
+                     }
                      this.serviceUrl = url;
+                     $('#url').val(this.serviceUrl);
                      console.log("Switched service url to " + this.serviceUrl);
-                 }).catch(function(err) {
+                     this.renderGroups();
+                 }).catch( (err) => {
                      console.log(err);
                      $('#error').text(err);
                  });
              } else {
-                 response.text().then(function(text) {
+                 response.text().then( (text) => {
                      $('#error').text(text);
                  });
              }
          }).catch(function(err) {
              $('#error').text(err);
          })
+  }
+  
+  renderGroups() {
+      fetch(this.serviceUrl + "group").then( (response) => {
+          if (response.ok) {
+              response.json().then( (json) => {
+                  //TODO NOW gotta be a better way than this
+                  //TODO NOW how detect & handle clicks on table? need to parameterize onclick
+                  let gtable = '<table><tr><th>ID</th><th>Name</th><th>Type</th><th>Owner</th><th>Created</th><th>Modified</th></tr>';
+                  for (const g of json) {
+                      const c = this.epochToDate(g.createdate);
+                      const m = this.epochToDate(g.moddate);
+                      gtable += `<tr><td>${g.id}</td><td>${g.name}</td><td>${g.type}</td><td>${g.owner}</td><td>${c}</td><td>${m}</td></tr>`;
+                  }
+                  gtable += '</table>';
+                  $('#groups').html(gtable);
+              }).catch( (err) => {
+                  $('#error').text(text);
+              });
+          } else {
+              response.text().then(function(text) {
+                  $('#error').text(text);
+              });
+          }
+      }).catch( (err) => {
+          $('#error').text(err);
+      });
   }
 };
