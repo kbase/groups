592ff14d0fe46820453bdb9778d9927a2615db1c
Gavin Price <gaprice@lbl.gov>
2018-10-29 17:33:15 -0700

    Add tests for WorkspaceID.

--- src/us/kbase/groups/core/workspace/WorkspaceHandler.java
+++ src/us/kbase/groups/core/workspace/WorkspaceHandler.java
@@ -4,15 +4,32 @@ import us.kbase.groups.core.UserName;
 import us.kbase.groups.core.exceptions.NoSuchWorkspaceException;
 import us.kbase.groups.core.exceptions.WorkspaceHandlerException;
 
+/** Handles requests for information from the KBase Workspace service.
+ * @author gaprice@lbl.gov
+ *
+ */
 public interface WorkspaceHandler {
 
-	//TODO JAVADOC
-
+	/** Determine whether a user is an administrator of a workspace.
+	 * @param wsid the ID of the workspace.
+	 * @param user the user.
+	 * @return true if the user is a workspace admin, false otherwise.
+	 * @throws WorkspaceHandlerException if an error occurs contacting the workspace.
+	 * @throws NoSuchWorkspaceException if the workspace is deleted or there is no workspace
+	 * with the given ID.
+	 */
 	boolean isAdministrator(WorkspaceID wsid, UserName user)
 			throws WorkspaceHandlerException, NoSuchWorkspaceException;
 
-	// always returns public workspaces regardless of bool
-	// pass null for user if anon user. In this case only public workspaces will be returned
+	/** Get information about a set of workspaces.
+	 * @param user the user requesting information. If null, indicating an anonymous user,
+	 * only public workspaces are returned.
+	 * @param ids the workspace IDs to get information about.
+	 * @param administratedWorkspacesOnly if true, only return public workspaces and workspaces
+	 * the user administrates. If false, return all workspaces (unless the user is null).
+	 * @return the information about the workspaces.
+	 * @throws WorkspaceHandlerException if an error occurs contacting the workspace.
+	 */
 	WorkspaceInfoSet getWorkspaceInformation(
 			UserName user,
 			WorkspaceIDSet ids,
