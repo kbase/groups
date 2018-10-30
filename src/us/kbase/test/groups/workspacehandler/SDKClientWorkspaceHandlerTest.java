9abf4410f8eb9716329e908ee77b5d72b35636dd
Gavin Price <gaprice@lbl.gov>
2018-10-30 16:46:28 -0700

    Finish workspace handler tests

--- src/us/kbase/test/groups/workspacehandler/SDKClientWorkspaceHandlerTest.java
+++ src/us/kbase/test/groups/workspacehandler/SDKClientWorkspaceHandlerTest.java
@@ -1,16 +1,19 @@
 package us.kbase.test.groups.workspacehandler;
 
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.argThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static us.kbase.test.groups.TestCommon.set;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 
 import org.junit.Test;
@@ -20,16 +23,22 @@ import com.google.common.collect.ImmutableMap;
 
 import us.kbase.common.service.JsonClientException;
 import us.kbase.common.service.ServerException;
+import us.kbase.common.service.Tuple9;
 import us.kbase.common.service.UObject;
 import us.kbase.groups.core.UserName;
 import us.kbase.groups.core.exceptions.NoSuchWorkspaceException;
 import us.kbase.groups.core.exceptions.WorkspaceHandlerException;
 import us.kbase.groups.core.workspace.WorkspaceID;
+import us.kbase.groups.core.workspace.WorkspaceIDSet;
+import us.kbase.groups.core.workspace.WorkspaceInfoSet;
+import us.kbase.groups.core.workspace.WorkspaceInformation;
 import us.kbase.groups.workspacehandler.SDKClientWorkspaceHandler;
 import us.kbase.test.groups.TestCommon;
 import us.kbase.workspace.WorkspaceClient;
 
 public class SDKClientWorkspaceHandlerTest {
+	
+	private static boolean DEBUG = false;
 	
 	private static class UObjectArgumentMatcher implements ArgumentMatcher<UObject> {
 
@@ -43,7 +52,7 @@ public class SDKClientWorkspaceHandlerTest {
 		public boolean matches(final UObject uo) {
 			final Object obj = uo.asInstance();
 			final boolean match = adminPackage.equals(obj);
-			if (!match) {
+			if (DEBUG && !match) {
 				System.out.println(String.format("UObject match failed. Expected:\n%s\nGot:\n%s",
 						adminPackage, obj));
 			}
@@ -122,16 +131,19 @@ public class SDKClientWorkspaceHandlerTest {
 		
 		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
 		
-		final Map<String, Object> command = ImmutableMap.of(
-				"command", "getPermissionsMass",
-				"params", ImmutableMap.of("workspaces", Arrays.asList(ImmutableMap.of("id", 24))));
-				
-		when(c.administer(argThat(new UObjectArgumentMatcher(command))))
+		when(c.administer(argThat(getPermissionsCommandMatcher(24))))
 				.thenReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
 						ImmutableMap.of("user1", "a", "user2", "w")))));
 		
 		assertThat("incorrect admin", h.isAdministrator(
 				new WorkspaceID(24), new UserName(user)), is(expected));
+	}
+
+	private UObjectArgumentMatcher getPermissionsCommandMatcher(final int wsid) {
+		return new UObjectArgumentMatcher(ImmutableMap.of(
+				"command", "getPermissionsMass",
+				"params", ImmutableMap.of("workspaces",
+						Arrays.asList(ImmutableMap.of("id", wsid)))));
 	}
 	
 	@Test
@@ -191,12 +203,7 @@ public class SDKClientWorkspaceHandlerTest {
 		
 		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
 		
-		final Map<String, Object> command = ImmutableMap.of(
-				"command", "getPermissionsMass",
-				"params", ImmutableMap.of("workspaces", Arrays.asList(ImmutableMap.of("id", 24))));
-				
-		when(c.administer(argThat(new UObjectArgumentMatcher(command))))
-				.thenThrow(exception);
+		when(c.administer(argThat(getPermissionsCommandMatcher(24)))).thenThrow(exception);
 		
 		failIsAdmin(h, new WorkspaceID(24), new UserName("user"), expected);
 	}
@@ -213,5 +220,302 @@ public class SDKClientWorkspaceHandlerTest {
 			TestCommon.assertExceptionCorrect(got, expected);
 		}
 	}
+	
+	@Test
+	public void getWorkspaceInformationNoWS() throws Exception {
+		final WorkspaceClient c = mock(WorkspaceClient.class);
+		
+		when(c.ver()).thenReturn("0.8.0");
+		
+		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
+		
+		final WorkspaceInfoSet wi = h.getWorkspaceInformation(
+				new UserName("u"),
+				WorkspaceIDSet.fromInts(set()),
+				false);
+		
+		assertThat("incorrect wsi", wi, is(WorkspaceInfoSet.getBuilder(new UserName("u"))
+				.build()));
+	}
 
+	@Test
+	public void getWorkspaceInformationFull() throws Exception {
+		getWorkspaceInformation(
+				new UserName("user1"),
+				false,
+				WorkspaceInfoSet.getBuilder(new UserName("user1"))
+						.withNonexistentWorkspace(8)
+						.withNonexistentWorkspace(9)
+						.withNonexistentWorkspace(20)
+						.withNonexistentWorkspace(21)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(5, "name5")
+								.withNullableNarrativeName("narr_name").build(),
+								true)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(7, "name7")
+								.build(),
+								false)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(10, "name10")
+								.withIsPublic(true).build(),
+								false)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(11, "name11")
+								.withIsPublic(true).build(),
+								false)
+						.build());
+	}
+	
+	@Test
+	public void getWorkspaceInformationAdministratedOnly() throws Exception {
+		getWorkspaceInformation(
+				new UserName("user1"),
+				true,
+				WorkspaceInfoSet.getBuilder(new UserName("user1"))
+						.withNonexistentWorkspace(9)
+						.withNonexistentWorkspace(20)
+						.withNonexistentWorkspace(21)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(5, "name5")
+								.withNullableNarrativeName("narr_name").build(),
+								true)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(10, "name10")
+								.withIsPublic(true).build(),
+								false)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(11, "name11")
+								.withIsPublic(true).build(),
+								false)
+						.build());
+	}
+	
+	@Test
+	public void getWorkspaceInformationAnonUser() throws Exception {
+		getWorkspaceInformationAnonUser(false);
+	}
+	
+	@Test
+	public void getWorkspaceInformationAnonUserAdministratedWSOnly() throws Exception {
+		getWorkspaceInformationAnonUser(true);
+	}
+
+	private void getWorkspaceInformationAnonUser(final boolean administratedWorkspacesOnly)
+			throws Exception {
+		getWorkspaceInformation(
+				null,
+				administratedWorkspacesOnly,
+				WorkspaceInfoSet.getBuilder(null)
+						.withNonexistentWorkspace(9)
+						.withNonexistentWorkspace(20)
+						.withNonexistentWorkspace(21)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(10, "name10")
+								.withIsPublic(true).build(),
+								false)
+						.withWorkspaceInformation(WorkspaceInformation.getBuilder(11, "name11")
+								.withIsPublic(true).build(),
+								false)
+						.build());
+	}
+
+	private void getWorkspaceInformation(
+			final UserName user,
+			final boolean administratedWorkspacesOnly,
+			final WorkspaceInfoSet expected)
+			throws Exception {
+		final WorkspaceClient c = mock(WorkspaceClient.class);
+		
+		when(c.ver()).thenReturn("0.8.0");
+		
+		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
+		
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "a", "user2", "w")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(5)));
+		
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "w", "user2", "w")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(7)));
+
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "w", "user2", "w")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(8)));
+
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "r", "user2", "w", "*", "r")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(9)));
+		
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "r", "user2", "w", "*", "r")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(10)));
+		
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "r", "user2", "w", "*", "r")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(11)));
+		
+		doThrow(new ServerException("Workspace 20 is deleted", -1, "n"))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(20)));
+		doThrow(new ServerException("No workspace with id 21 exists", -1, "n"))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(21)));
+
+		doReturn(getWorkspaceInfoResponse(5, "name5", false, ImmutableMap.of(
+				"is_temporary", "false",
+				"narrative_nice_name", "narr_name")))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(5)));
+
+		doReturn(getWorkspaceInfoResponse(7, "name7", false, ImmutableMap.of(
+				"is_temporary", "true",
+				"narrative_nice_name", "narr_name2")))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(7)));
+		
+		doThrow(new ServerException("Workspace 8 is deleted", -1, "n"))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(8)));
+		doThrow(new ServerException("No workspace with id 9 exists", -1, "n"))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(9)));
+		
+		doReturn(getWorkspaceInfoResponse(10, "name10", true, ImmutableMap.of(
+				"is_temporary", "false")))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(10)));
+
+		doReturn(getWorkspaceInfoResponse(11, "name11", true, Collections.emptyMap()))
+				.when(c).administer(argThat(getWSInfoCommandMatcher(11)));
+		
+		final WorkspaceInfoSet wi = h.getWorkspaceInformation(
+				user,
+				WorkspaceIDSet.fromInts(set(5, 7, 8, 9, 10, 11, 20, 21)),
+				administratedWorkspacesOnly);
+		
+		assertThat("incorrect wsinfo", wi, is(expected));
+	}
+
+	private UObjectArgumentMatcher getWSInfoCommandMatcher(final int wsid) {
+		return new UObjectArgumentMatcher(ImmutableMap.of(
+				"command", "getWorkspaceInfo",
+				"params", ImmutableMap.of("id", wsid)));
+	}
+	
+	private UObject getWorkspaceInfoResponse(
+			final int id,
+			final String name,
+			final boolean isPublic,
+			final Map<String, String> meta) {
+		return new UObject(new Tuple9<Long, String, String, String, Long, String, String, String,
+				Map<String, String>>()
+				.withE1((long) id)
+				.withE2(name)
+				.withE7(isPublic ? "r" : "n")
+				.withE9(meta));
+		// other fields are currently unused in the handler
+	}
+	
+	@Test
+	public void getWorkspaceInfoFailNull() throws Exception {
+		final WorkspaceClient c = mock(WorkspaceClient.class);
+		
+		when(c.ver()).thenReturn("0.8.0");
+		
+		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
+		
+		failGetWorkspaceInfo(h, null, new NullPointerException("ids"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailPermsOtherServerException() throws Exception {
+		failGetWorkspaceInfoOnPermissionsCall(
+				new ServerException("You pootied real bad I can smell it", -1, "n"),
+				new WorkspaceHandlerException("Error contacting workspace at http://baz.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailPermsJsonClientException() throws Exception {
+		failGetWorkspaceInfoOnPermissionsCall(
+				new JsonClientException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://baz.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailPermsIOException() throws Exception {
+		failGetWorkspaceInfoOnPermissionsCall(
+				new IOException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://baz.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailPermsIllegalStateException() throws Exception {
+		failGetWorkspaceInfoOnPermissionsCall(
+				new IllegalStateException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://baz.com"));
+	}
+	
+	private void failGetWorkspaceInfoOnPermissionsCall(
+			final Exception thrown,
+			final Exception expected)
+			throws Exception {
+		final WorkspaceClient c = mock(WorkspaceClient.class);
+		
+		when(c.ver()).thenReturn("0.8.0");
+		when(c.getURL()).thenReturn(new URL("http://baz.com"));
+		
+		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
+		
+		when(c.administer(argThat(getPermissionsCommandMatcher(24)))).thenThrow(thrown);
+		
+		failGetWorkspaceInfo(h, WorkspaceIDSet.fromInts(set(24)), expected);
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailGetWSOtherServerException() throws Exception {
+		failGetWorkspaceInfoOnGetWSCall(
+				new ServerException("You pootied real bad I can smell it", -1, "n"),
+				new WorkspaceHandlerException("Error contacting workspace at http://bat.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailGetWSJsonClientException() throws Exception {
+		failGetWorkspaceInfoOnGetWSCall(
+				new JsonClientException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://bat.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailGetWSIOException() throws Exception {
+		failGetWorkspaceInfoOnGetWSCall(
+				new IOException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://bat.com"));
+	}
+	
+	@Test
+	public void getWorkspaceInformationFailGetWSIllegalStateException() throws Exception {
+		failGetWorkspaceInfoOnGetWSCall(
+				new IllegalStateException("You pootied real bad I can smell it"),
+				new WorkspaceHandlerException("Error contacting workspace at http://bat.com"));
+	}
+	
+	private void failGetWorkspaceInfoOnGetWSCall(
+			final Exception thrown,
+			final Exception expected)
+			throws Exception {
+		final WorkspaceClient c = mock(WorkspaceClient.class);
+		
+		when(c.ver()).thenReturn("0.8.0");
+		when(c.getURL()).thenReturn(new URL("http://bat.com"));
+		
+		final SDKClientWorkspaceHandler h = new SDKClientWorkspaceHandler(c);
+		
+		doReturn(new UObject(ImmutableMap.of("perms", Arrays.asList(
+				ImmutableMap.of("user1", "a", "*", "r")))))
+				.when(c).administer(argThat(getPermissionsCommandMatcher(24)));
+
+		doThrow(thrown).when(c).administer(argThat(getWSInfoCommandMatcher(24)));
+		
+		failGetWorkspaceInfo(h, WorkspaceIDSet.fromInts(set(24)), expected);
+	}
+	
+	private void failGetWorkspaceInfo(
+			final SDKClientWorkspaceHandler h,
+			final WorkspaceIDSet ids,
+			final Exception expected) {
+		try {
+			// no way to cause a fail via user or adminOnly param
+			h.getWorkspaceInformation(null, ids, false);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, expected);
+		}
+	}
+	
 }
