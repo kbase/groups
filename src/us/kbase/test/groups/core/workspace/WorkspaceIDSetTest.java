e12594094d1f16758f897ca484ac4f6ccb0665f7
Gavin Price <gaprice@lbl.gov>
2018-10-31 15:48:00 -0700

    Missing test

--- src/us/kbase/test/groups/core/workspace/WorkspaceIDSetTest.java
+++ src/us/kbase/test/groups/core/workspace/WorkspaceIDSetTest.java
@@ -85,13 +85,22 @@ public class WorkspaceIDSetTest {
 	
 	@Test
 	public void contains() throws Exception {
-		final WorkspaceIDSet wsids = WorkspaceIDSet.fromIDs(
-				set(new WorkspaceID(1), new WorkspaceID(23)));
+		final WorkspaceIDSet wsids = WorkspaceIDSet.fromInts(set(1, 23));
 		
 		assertThat("incorrect contains", wsids.contains(new WorkspaceID(1)), is(true));
 		assertThat("incorrect contains", wsids.contains(new WorkspaceID(23)), is(true));
 		assertThat("incorrect contains", wsids.contains(new WorkspaceID(2)), is(false));
 		assertThat("incorrect contains", wsids.contains(new WorkspaceID(22)), is(false));
 		assertThat("incorrect contains", wsids.contains(new WorkspaceID(24)), is(false));
+	}
+	
+	@Test
+	public void containsFail() {
+		try {
+			WorkspaceIDSet.fromInts(set()).contains(null);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, new NullPointerException("wsid"));
+		}
 	}
 }
