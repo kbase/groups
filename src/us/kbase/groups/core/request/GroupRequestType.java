aa9665ae31347da8ffc8fe08db622e742d36f7ab
Gavin Price <gaprice@lbl.gov>
2018-10-30 19:03:44 -0700

    Add workspace ID to GroupRequest.

--- src/us/kbase/groups/core/request/GroupRequestType.java
+++ src/us/kbase/groups/core/request/GroupRequestType.java
@@ -8,8 +8,14 @@ public enum GroupRequestType {
 
 	// NOTE the enum values are stored in the DB. Don't change them.
 	
+	/** Invite a user to a group you administrate. */
 	INVITE_TO_GROUP				("Invite to group"),
-	REQUEST_GROUP_MEMBERSHIP	("Request group membership");
+	/** Request membership to a group. */
+	REQUEST_GROUP_MEMBERSHIP	("Request group membership"),
+	/** Invite a workspace (via its administrators) to a group you administrate. */
+	INVITE_WORKSPACE			("Invite workspace to group"),
+	/** Request that a workspace you administrate is added to a group. */
+	REQUEST_ADD_WORKSPACE		("Request add workspace to group");
 	
 	private final String representation;
 	
