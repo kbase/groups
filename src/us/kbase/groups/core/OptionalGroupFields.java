ec9cc199b71fb774a83e151898c8362bd6e390fa
Gavin Price <gaprice@lbl.gov>
2018-11-02 13:48:25 -0700

    Add OptionalGroupFields class
    Just description for now, but will add more fields shortly

---
+++ src/us/kbase/groups/core/OptionalGroupFields.java
@@ -0,0 +1,113 @@
+package us.kbase.groups.core;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static us.kbase.groups.util.Util.checkString;
+
+import us.kbase.groups.core.FieldItem.StringField;
+import us.kbase.groups.core.exceptions.IllegalParameterException;
+import us.kbase.groups.core.exceptions.MissingParameterException;
+
+/** Optional fields associated with a {@link Group}. 
+ * @author gaprice@lbl.gov
+ *
+ */
+public class OptionalGroupFields {
+	
+	private final FieldItem<String> description;
+
+	private OptionalGroupFields(FieldItem<String> description) {
+		this.description = description;
+	}
+
+	/** Get the description of the group.
+	 * @return the description.
+	 */
+	public FieldItem<String> getDescription() {
+		return description;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		OptionalGroupFields other = (OptionalGroupFields) obj;
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		return true;
+	}
+	
+	/** Get a builder for a {@link OptionalGroupFields}.
+	 * @return the builder.
+	 */
+	public static Builder getBuilder() {
+		return new Builder();
+	}
+	
+	/** Get the default set of {@link OptionalGroupFields}, where all of the fields are set to
+	 * {@link FieldItem#noAction()}.
+	 * @return the fields.
+	 */
+	public static OptionalGroupFields getDefault() {
+		return getBuilder().build();
+	}
+	
+	/** A builder for a {@link OptionalGroupFields}.
+	 * @author gaprice@lbl.gov
+	 *
+	 */
+	public static class Builder {
+
+		private FieldItem<String> description = FieldItem.noAction();
+		
+		private Builder() {}
+		
+		/** Add a group description. The maximum description size is
+		 * {@link Group#MAX_DESCRIPTION_CODE_POINTS} Unicode code points.
+		 * @param description the new description.
+		 * @return this builder.
+		 * @throws IllegalParameterException if the description is too long.
+		 */
+		public Builder withDescription(final StringField description)
+				throws IllegalParameterException {
+			checkNotNull(description, "description");
+			if (description.hasItem()) {
+				try {
+					checkString(description.get(), "description",
+							Group.MAX_DESCRIPTION_CODE_POINTS);
+				} catch (MissingParameterException e) {
+					throw new RuntimeException("This should be impossible");
+				}
+			}
+			this.description = description;
+			return this;
+		}
+		
+		/** Build the {@link OptionalGroupFields}.
+		 * @return the fields.
+		 */
+		public OptionalGroupFields build() {
+			return new OptionalGroupFields(description);
+		}
+	}
+	
+}
---
+++ src/us/kbase/test/groups/core/OptionalGroupFieldsTest.java
@@ -0,0 +1,82 @@
+package us.kbase.test.groups.core;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import us.kbase.groups.core.FieldItem;
+import us.kbase.groups.core.OptionalGroupFields;
+import us.kbase.groups.core.FieldItem.StringField;
+import us.kbase.groups.core.exceptions.IllegalParameterException;
+import us.kbase.test.groups.TestCommon;
+
+public class OptionalGroupFieldsTest {
+	
+	@Test
+	public void equals() throws Exception {
+		EqualsVerifier.forClass(OptionalGroupFields.class).usingGetClass().verify();
+	}
+	
+	@Test
+	public void getDefault() throws Exception {
+		final OptionalGroupFields ofg = OptionalGroupFields.getDefault();
+		
+		assertThat("incorrect desc", ofg.getDescription(), is(FieldItem.noAction()));
+	}
+	
+	@Test
+	public void buildMinimal() throws Exception {
+		final OptionalGroupFields ofg = OptionalGroupFields.getBuilder().build();
+		
+		assertThat("incorrect desc", ofg.getDescription(), is(FieldItem.noAction()));
+	}
+	
+	@Test
+	public void buildWithDescription() throws Exception {
+		final OptionalGroupFields ofg = OptionalGroupFields.getBuilder()
+				.withDescription(StringField.from("   foo    "))
+				.build();
+		
+		assertThat("incorrect desc", ofg.getDescription(), is(FieldItem.from("foo")));
+	}
+	
+	@Test
+	public void buildWithRemoveDescription() throws Exception {
+		final OptionalGroupFields ofg = OptionalGroupFields.getBuilder()
+				.withDescription(StringField.remove())
+				.build();
+		
+		assertThat("incorrect desc", ofg.getDescription(), is(FieldItem.remove()));
+	}
+	
+	@Test
+	public void withDescriptionFail() throws Exception {
+		failWithDescription(null, new NullPointerException("description"));
+		
+		final String uni = "a₸𐍆"; // length 5, 4 code points, 11 bytes in utf-8
+		final StringBuilder b = new StringBuilder();
+		for (int i = 0; i < 1250; i++) {
+			b.append(uni);
+		}
+		
+		// should pass
+		OptionalGroupFields.getBuilder().withDescription(StringField.from(b.toString()));
+		
+		b.append("a");
+		failWithDescription(StringField.from(b.toString()), new IllegalParameterException(
+				"description size greater than limit 5000"));
+	}
+
+	private void failWithDescription(final StringField desc, final Exception expected) {
+		try {
+			OptionalGroupFields.getBuilder().withDescription(desc);
+			fail("expected exception");
+		} catch (Exception got) {
+			TestCommon.assertExceptionCorrect(got, expected);
+		}
+	}
+
+}
