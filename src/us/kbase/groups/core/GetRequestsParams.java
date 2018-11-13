0665e40583393ede7adb3ee253cf56f08cc1d943
Gavin Price <gaprice@lbl.gov>
2018-11-12 16:40:02 -0800

    Add tests for GetRequestsParams.

--- src/us/kbase/groups/core/GetRequestsParams.java
+++ src/us/kbase/groups/core/GetRequestsParams.java
@@ -3,11 +3,15 @@ package us.kbase.groups.core;
 import java.time.Instant;
 import java.util.Optional;
 
+import us.kbase.groups.core.request.GroupRequest;
+import us.kbase.groups.core.request.GroupRequestStatusType;
+
+/** A set of parameters for use when listing {@link GroupRequest}s.
+ * @author gaprice@lbl.gov
+ *
+ */
 public class GetRequestsParams {
 
-	// TODO JAVADOC
-	// TODO TEST
-	
 	private final boolean includeClosed;
 	private final boolean sortAscending;
 	private final Optional<Instant> excludeUpTo;
@@ -21,14 +25,26 @@ public class GetRequestsParams {
 		this.excludeUpTo = excludeUpTo;
 	}
 
+	/** Get whether closed requests should be included in the list. Any request with a status type
+	 * other than {@link GroupRequestStatusType#OPEN} is closed.
+	 * @return whether closed requests should be included.
+	 */
 	public boolean isIncludeClosed() {
 		return includeClosed;
 	}
 
+	/** Get whether the list should be sorted in ascending or descending order.
+	 * @return true if the sort should be ascending, false if descending.
+	 */
 	public boolean isSortAscending() {
 		return sortAscending;
 	}
 
+	/** Get a date that determines where a list of requests should begin. If the sort is
+	 * ascending, the request list should begin at a date strictly later than this date, and
+	 * vice versa for descending sorts.
+	 * @return the exlusion date.
+	 */
 	public Optional<Instant> getExcludeUpTo() {
 		return excludeUpTo;
 	}
@@ -71,10 +87,17 @@ public class GetRequestsParams {
 		return true;
 	}
 	
+	/** Get a builder for a {@link GetRequestsParams}.
+	 * @return the builder.
+	 */
 	public static Builder getBuilder() {
 		return new Builder();
 	}
 	
+	/** A builder for a {@link GetRequestsParams}.
+	 * @author gaprice@lbl.gov
+	 *
+	 */
 	public static class Builder {
 		
 		private boolean includeClosed = false;
@@ -83,6 +106,12 @@ public class GetRequestsParams {
 		
 		private Builder() {}
 		
+		/** Set whether closed requests should be included in the returned list.
+		 * Any request with a status type other than {@link GroupRequestStatusType#OPEN} is closed.
+		 * If null, the default of false is used.
+		 * @param includeClosed true to include closed requests, false otherwise.
+		 * @return this builder.
+		 */
 		public Builder withNullableIncludeClosed(final Boolean includeClosed) {
 			if (includeClosed == null) {
 				this.includeClosed = false;
@@ -92,20 +121,35 @@ public class GetRequestsParams {
 			return this;
 		}
 		
+		/** Set whether the list should be sorted in ascending or descending order.
+		 * If null, the default of true is used.
+		 * @param sortAscending true sort ascending, false for descending.
+		 * @return this builder.
+		 */
 		public Builder withNullableSortAscending(final Boolean sortAscending) {
 			if (sortAscending == null) {
-				this.sortAscending = false;
+				this.sortAscending = true;
 			} else {
 				this.sortAscending = sortAscending;
 			}
 			return this;
 		}
 		
+		/** Set a date that determines where a list of requests should begin. If the sort is
+		 * ascending, the request list should begin at a date strictly later than this date, and
+		 * vice versa for descending sorts.
+		 * If null, no date is set.
+		 * @param excludeUpTo the exclusion date.
+		 * @return this builder.
+		 */
 		public Builder withNullableExcludeUpTo(final Instant excludeUpTo) {
 			this.excludeUpTo = Optional.ofNullable(excludeUpTo);
 			return this;
 		}
 		
+		/** Build the {@link GetRequestsParams}.
+		 * @return the parameters.
+		 */
 		public GetRequestsParams build() {
 			return new GetRequestsParams(includeClosed, sortAscending, excludeUpTo);
 		}
